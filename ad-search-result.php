506fc11984a95af5f341879b00cb57c423ce9278
Alejandro Palomino García <alejandrodh570@gmail.com>
2020-01-25 18:36:39 +0100

    Commit 035 -> Actualizado mapa con búsqueda por ubicación y FIX en anuncios sin fotos.

--- ad-search-result.php
+++ ad-search-result.php
@@ -2,8 +2,9 @@
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 <link href="assets/css/style-ad-search.css" rel="stylesheet">
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="assets/css/googleMapsStyle.css">
 
-<section class="search-box">
+<section class=" search-box">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 listing-block">
@@ -32,12 +33,12 @@
                         $i++;
                     } while ($i < 6 && !$imagenEncontrada);
 
-                    echo "<div class='media'<br>";
+                    echo "<a href='ad-show.php?id=" . $registro['idAnuncio'] . "' target='_blank'><div class='media'<br>";
                     if ($imagenEncontrada) {
-                        echo "<a href='ad-show.php?id=" . $registro['idAnuncio'] . "' target='_blank'><img class='d-flex align-self-start' src='" . $urlFoto . "' alt='Generic placeholder image'></a>";
+                        echo "<img class='d-flex align-self-start' src='" . $urlFoto . "' alt='Generic placeholder image'>";
                         $imagenEncontrada = false;
                     } else {
-                        echo "<a href='ad-show.php?id=" . $registro['idAnuncio'] . "' target='_blank'><img class='d-flex align-self-start' src='assets/img/ads/sinImagen.jpg' alt='Generic placeholder image'></a>";
+                        echo "<img class='d-flex align-self-start' src='assets/img/ads/sinImagen.jpg' alt='Generic placeholder image'>";
                     }
 
                     echo "<div class='media-body pl-3'>";
@@ -49,22 +50,26 @@
                     echo "</div>";
                     echo "<div class='address'>" . $registro['direccion'] . "</div>";
                     echo "</div>";
-                    echo "</div>";
+                    echo "</div></a>";
                 }
 
                 ?>
             </div>
-            <div class="col-md-7 map-box mx-0 px-0">
-                <iframe width="100%" height="495" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=Sevilla%2C%20Spain+(Mapa)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=B&amp;output=embed"></iframe>
+
+            <div id="map" class="col-md-7 map-box mx-0 px-0">
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAj5O6tEn8iirebfPdhi314wuqGyLjoeCA&libraries=places&callback=initAutocomplete" async defer></script>
             </div>
+            <input id="pac-input" class="controls" type="text" placeholder="¡Busca la ubicación de tu inmueble!">
+
         </div>
     </div>
 </section>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jQuery-slimScroll/1.3.8/jquery.slimscroll.min.js"></script>
+<script src="assets/js/mapJs.js"></script>
 <script>
     $(function() {
         $('.listing-block').slimScroll({
-            height: '500px'
+            height: '500px',
         });
     });
 </script>
